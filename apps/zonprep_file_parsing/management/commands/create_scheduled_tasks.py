6ba5949cd24c490d9acec30ba8bcf38bca85edd0
Daniel Mouris <dgmouris@gmail.com>
2024-08-20 15:16:15 -0600

    Changed the management command to be a bit better

--- apps/zonprep_file_parsing/management/commands/create_scheduled_tasks.py
+++ apps/zonprep_file_parsing/management/commands/create_scheduled_tasks.py
@@ -1,9 +1,7 @@
 from django.core.management.base import BaseCommand
 from django_celery_beat.models import PeriodicTask, IntervalSchedule
 
-from apps.zonprep_file_parsing.models import ZonprepAppointment
-
-
+from django.conf import settings
 
 
 class Command(BaseCommand):
@@ -14,38 +12,57 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Creating Tasks...")
-        # Create the standard task schedule.
-        schedule, _ = IntervalSchedule.objects.get_or_create(
-            every=10,
-            period=IntervalSchedule.MINUTES, # for production.
-            # period=IntervalSchedule.SECONDS, # for development note: remove this in the future.
-        )
-
+        # Create the standard task schedule
+        # shorter interval for development.
+        schedule = None
+        if settings.DEBUG:
+            schedule, _ = IntervalSchedule.objects.get_or_create(
+                every=10,
+                period=IntervalSchedule.SECONDS, # for development note: remove this in the future.
+            )
+        else:
+            schedule, _ = IntervalSchedule.objects.get_or_create(
+                every=5,
+                period=IntervalSchedule.MINUTES, # for production.
+            )
+        
         # Create Task: Send Email To Fulfilment Worker
         name = "Send Email To Fulfilment Worker"
-        send_out_emails_task = PeriodicTask.objects.create(
-            name='Send Email To Fulfilment Worker',
-            task='apps.zonprep_file_parsing.tasks.send_out_appointment_isa_emails_to_fulfillment_task',
-            interval=schedule,
-        )
+        
+        already_exists = self._does_task_exist(name)
+        
+        if not already_exists:
+            send_out_emails_task = PeriodicTask.objects.create(
+                name='Send Email To Fulfilment Worker',
+                task='apps.zonprep_file_parsing.tasks.send_out_appointment_isa_emails_to_fulfillment_task',
+                interval=schedule,
+            )
 
-        if send_out_emails_task:
-            self.stdout.write(F"Task: '{send_out_emails_task.name}' Successfully")
+            if send_out_emails_task:
+                self.stdout.write(F"Task: '{send_out_emails_task.name}' Successfully")
+            else:
+                self.stdout.write(F"Task: '{name}' Creation Failed")
         else:
-            self.stdout.write(F"Task: '{name}' Creation Failed")
-
+            self.stdout.write(F"Task: '{name}' Already Exists, nothing to do.")
 
         # Create Task: Parsing Type A Email Attachment Worker
         name = "Parsing Type A Email Attachment Worker"
-        parse_type_a_email_attachments_task = PeriodicTask.objects.create(
-            name='Parsing Type A Email Attachment Worker',
-            task='apps.zonprep_file_parsing.tasks.parse_type_a_email_attachments_task',
-            interval=schedule,
-        )
+        
+        already_exists = self._does_task_exist(name)
+        if not already_exists:
+            parse_type_a_email_attachments_task = PeriodicTask.objects.create(
+                name='Parsing Type A Email Attachment Worker',
+                task='apps.zonprep_file_parsing.tasks.parse_type_a_email_attachments_task',
+                interval=schedule,
+            )
 
-        if parse_type_a_email_attachments_task:
-            self.stdout.write(F"Task: '{parse_type_a_email_attachments_task.name}' Successfully")
+            if parse_type_a_email_attachments_task:
+                self.stdout.write(F"Task: '{parse_type_a_email_attachments_task.name}' Successfully")
+            else:
+                self.stdout.write(F"Task: '{name}' Creation Failed")
         else:
-            self.stdout.write(F"Task: '{name}' Creation Failed")
+            self.stdout.write(F"Task: '{name}' Already Exists, nothing to do.")
 
 
+    def _does_task_exist(self, task_name):
+        return PeriodicTask.objects.filter(name=task_name).exists()
