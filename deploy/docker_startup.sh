9a8d9bcb21c7996bbfc1a0834b46ffb4705d378a
Daniel Mouris <dgmouris@gmail.com>
2024-11-17 16:48:30 -0700

    Added one more task for the start up

--- deploy/docker_startup.sh
+++ deploy/docker_startup.sh
@@ -10,5 +10,7 @@ echo "Environment Vars"
 printenv
 echo "Django migrate"
 python manage.py migrate --noinput
+echo "Create scheduled tasks (if needed)"
+python manage.py create_scheduled_tasks
 echo "Run Gunicorn"
 gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 8 --timeout 0 zon_prep_ocr_project.wsgi:application
